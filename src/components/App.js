7cd0ef0fdd22e8e3e966f5e69af705335869252a
Pro-shanto06 <proshantosaha1999@gmail.com>
2024-04-25 22:16:26 +0600

    done

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState(() => {
+    const storedItems = localStorage.getItem("packingListItems");
+    return storedItems ? JSON.parse(storedItems) : [];
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem("packingListItems", JSON.stringify(items));
+  }, [items]);
+
+  function handleAddItems(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
